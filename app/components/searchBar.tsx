f63f659bd067276400f6083b28b5e8e7c1096932
Teran <teranarthur99@gmail.com>
2025-10-09 18:43:05 -0700

    configured for typescript, using latest versions

--- app/components/searchBar.tsx
+++ app/components/searchBar.tsx
@@ -1,3 +1,14 @@
+'use client';
+import { Form } from 'react-bootstrap';
+
+export default function SearchBar() {
+  return (
+    <div className="search-bar">
+      <Form.Control />
+    </div>
+  );
+}
+
 // 'use client';
 // import React from 'react';
 // import { useState } from 'react';
